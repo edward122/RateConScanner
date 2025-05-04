147109125da6468d5b4c86334de17ad6a064d8ca
Sky Market <kisiledw12345@gmail.com>
2025-05-04 14:06:28 -0700

    1.4
    trying hosting on github

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,25 @@
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/RateConScanner' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/RateConScanner/' : '',
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'picsum.photos',
+      port: '',
+      pathname: '/**',
+    },
+  ],
 };
 
 export default nextConfig;
